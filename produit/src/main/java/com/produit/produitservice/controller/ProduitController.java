8738cb2968ab8fb97a4d72222595a27f2b88f36a
Moussa Niasse <niassemoussa94@gmail.com>
2025-02-27 01:27:49 +0000

    Initialisation du repo

---
+++ produit/src/main/java/com/produit/produitservice/controller/ProduitController.java
@@ -0,0 +1,28 @@
+package com.produit.produitservice.controller;
+
+import com.produit.produitservice.model.Produit;
+import com.produit.produitservice.service.ProduitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/produits")
+@RequiredArgsConstructor
+public class ProduitController {
+    private final ProduitService produitService;
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "Hello world";
+    }
+    @GetMapping("/allproduit")
+    public List<Produit> getAllProduits()
+    {
+        return produitService.getAllProduits();
+    }
+    @PostMapping
+    public Produit createProduit(@RequestBody Produit produit){
+        return produitService.createProduit(produit);
+    }
+}
